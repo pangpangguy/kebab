2a8f2159cf0d7651fbaf126bb2c72cdf76466909
pangpangguy <ryanfx1998@gmail.com>
2021-06-26 22:00:52 +0200

    merge

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const Kebab = require("./models/kebab");
-const methodOverride = require("method-override")
+const methodOverride = require("method-override");
 
 //Database
 mongoose.connect("mongodb://localhost:27017/kebab-camp", {
@@ -22,46 +22,51 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded({extended:true}))
-app.use(methodOverride("_method"))
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/kebabs", async (req,res)=>{
+app.get("/kebabs", async (req, res) => {
   const allKebabs = await Kebab.find({});
-  res.render("kebabs/index", {allKebabs})
-})
+  res.render("kebabs/index", { allKebabs });
+});
 
-app.get("/kebabs/new", (req,res)=>{
-  res.render("kebabs/new")
-})
+app.get("/kebabs/new", (req, res) => {
+  res.render("kebabs/new");
+});
 
-app.get("/kebabs/:id", async (req,res) =>{
-  const {id} = req.params;
-  const kebab = await Kebab.findById(id)
-  res.render("kebabs/show", {kebab})  
-})
+app.get("/kebabs/:id", async (req, res) => {
+  const { id } = req.params;
+  const kebab = await Kebab.findById(id);
+  res.render("kebabs/show", { kebab });
+});
 
+app.post("/kebabs", async (req, res) => {
+  const newKebab = await new Kebab(req.body.kebab);
+  newKebab.save();
+  res.redirect("/kebabs");
+});
 
-app.post("/kebabs", async(req,res)=>{
-  const newKebab = await new Kebab(req.body.kebab)
-  newKebab.save()
-  res.redirect("/kebabs")
-})
+app.get("/kebabs/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const kebab = await Kebab.findById(id);
+  res.render("kebabs/edit", { kebab });
+});
 
-app.get("/kebabs/:id/edit", async (req,res)=>{
-  const {id} = req.params
-  const kebab = await Kebab.findById(id)
-  res.render("kebabs/edit", {kebab})
-})
+app.put("/kebabs/:id", async (req, res) => {
+  const { id } = req.params;
+  const updatedKebab = await Kebab.findByIdAndUpdate(id, { ...req.body.kebab });
+  res.redirect(`/kebabs/${updatedKebab._id}`);
+});
 
-app.put("/kebabs/:id", async (req,res) =>{
-  const {id} = req.params
-  const updatedKebab = await Kebab.findByIdAndUpdate(id, {...req.body.kebab})
-  res.redirect(`/kebabs/${updatedKebab._id}`)
-})
+app.delete("/kebabs/:id", async (req, res) => {
+  const { id } = req.params;
+  await Kebab.findByIdAndDelete(id);
+  res.redirect("/kebabs");
+});
 
 app.listen(3000, () => {
   console.log("Listening on port 3000...");
